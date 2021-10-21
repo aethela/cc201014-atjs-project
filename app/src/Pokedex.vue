309c6da4ad83465c6455b94813281fa9bb0cede2
Sophia Gruber <cc201014@fhstp.ac.at>
2021-10-21 19:27:37 +0200

    Added Pokedex

---
+++ app/src/Pokedex.vue
@@ -0,0 +1,44 @@
+<script>
+export default {
+    data() {
+        return {
+            list: []
+        }
+    },
+
+    methods: {
+        async fetchPokemon() {
+         const url = 'https://pokeapi.co/api/v2/pokemon?limit=151'
+
+         const data = await fetch(url)
+
+         const pokemon = await data.json()
+
+         console.log(pokemon);
+
+         this.list = pokemon.results;
+        }
+    }
+}
+</script>
+
+<template>
+
+    <div>
+        <h1>Pokedex</h1>
+
+        <button @click="fetchPokemon">Catch Pokemon</button>
+
+        <div v-for="(item, index) in list" :key="index">
+            <div>
+                <img :src="`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${ index + 1 }.png`" alt="" srcset="">
+                <h2>{{ item.name }}</h2>
+            </div>
+
+        </div>
+
+        <!-- {{ list }} -->
+    </div>
+</template>
+
+
--- app/src/main.js
+++ app/src/main.js
@@ -1,4 +1,20 @@
 import { createApp } from 'vue'
+import { createRouter, createWebHistory } from 'vue-router'
 import App from './App.vue'
+import Home from './Home.vue'
+import Pokedex from './Pokedex.vue'
 
-createApp(App).mount('#app')
+// there is also createWebHashHistory and createMemoryHistory
+
+const router = createRouter({
+  history: createWebHistory(),
+  routes: [  
+    { path: '/', component: Home }, 
+    { path: '/pokedex', component: Pokedex  }
+],
+})
+
+const app = createApp({})
+
+app.use(router)
+app.mount('#app')
